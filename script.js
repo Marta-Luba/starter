be841128b62ee801fde27d3cd0a3f442073712e5
Marta Luba <marta.luba@yahoo.co.uk>
2023-02-08 19:02:37 +0000

    more js

--- script.js
+++ script.js
@@ -1,12 +1,88 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const x = 23;
-if (x === 23) console.log(23);
+// const x = 23;
+// if (x === 23) console.log(23);
 
-const calcAge = (birthYear) => 2023 - birthYear;
-console.log(calcAge(1984));
+// const calcAge = (birthYear) => 2023 - birthYear;
+// console.log(calcAge(1984));
 
-console.log("hello world");
-console.log();
-console.log();
+// console.log("hello world");
+// console.log();
+// console.log();
+/*
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmpNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+calcTempAmpNew([3, 7, 4]);
+const amplitude = calcTempAmpNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    // value: Number(prompt("Degrees celsius:")),
+    value: 10,
+  };
+  console.log(measurement);
+  console.log(measurement.value);
+  console.table(measurement);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+console.log(measureKelvin());
+30;
+
+const calcTempAmpBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmpBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
+*/
+
+const maxTemps1 = [17, 21, 23];
+const maxTemps2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}°C in ${i + 1} days ... `;
+  }
+  console.log("... " + str);
+};
+printForecast(maxTemps1);
+//printForecast(maxTemps2);
